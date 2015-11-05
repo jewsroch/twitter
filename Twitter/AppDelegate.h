658d1f168bd22df8ea5b7bed64305e1d9a59f583
Chad Jewsbury <cjewsbury@groupon.com>
2015-11-04 18:28:55 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Chad Jewsbury on 11/4/15.
+//  Copyright © 2015 Chad Jewsbury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Chad Jewsbury on 11/4/15.
+//  Copyright © 2015 Chad Jewsbury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
